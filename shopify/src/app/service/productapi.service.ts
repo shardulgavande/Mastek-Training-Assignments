9b8ae6afd978bb56607889f79e8a8b0b55645e74
Shardul Gavande <shardulgavande@gmail.com>
2022-01-23 20:26:11 +0530

    assginment postgre
    assginment postgre

--- shopify/src/app/service/productapi.service.ts
+++ shopify/src/app/service/productapi.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders  } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { IProducts } from '../IProducts';
 
 @Injectable({
@@ -11,8 +11,21 @@ export class ProductapiService {
   http_option = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
+  
   constructor(private httpClient:HttpClient) { }
   getProducts():Observable<IProducts []>{
     return this.httpClient.get<IProducts[]>(this.base_url+"/products");
   }
+  handleError(error:HttpErrorResponse){
+    if(error.error instanceof ErrorEvent){
+      console.error('An error occurred:',error.error.message);
+    }
+    else{
+      console.error(`Backend returned code ${error.status},` +
+      `body was:  ${error.error}`);
+    }
+    return throwError(
+      `Something bad happened; please try again later`
+    );
+  };
 }
